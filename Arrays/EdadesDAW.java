e3551bd72a71e2585ef46ef32d1b062b378f15ef
viceentbertet <perellobertetjosepvicent@gmail.com>
2023-12-13 19:25:22 +0100

    EdadesDAW completado (sin corregir)

--- Arrays/EdadesDAW.java
+++ Arrays/EdadesDAW.java
@@ -3,16 +3,42 @@ package Arrays;
 public class EdadesDAW {
     public static void main(String[] args){
 
-        String claseDAW[][] = {{"Ricardo", "Jesús", "Victor", "Guillem", "Pablo", "Érika", "Filip"}, {"Arturo", "Fernando", "Kevin", "Tavi", "Miguel", "Hugo R", "Alberto"}, {"Izan", "Jordi", "Hugo B", "Alex", "Vicent"}};
-        int edadesDAW[][]= {{20, 20, 19, 19, 28, 17, 19},{31, 29, 24, 21, 19, 19, 19},{18, 20, 18, 23, 18}};
+        String[][] classDAW = {{"Ricardo", "Jesús", "Victor", "Guillem", "Pablo", "Érika", "Filip"},{"Arturo", "Fernando", "Kevin", "Tavi", "Miguel", "Hugo R", "Alberto"}, {"Izan", "Jordi", "Hugo B", "Alex", "Vicent"}};
+        int[][] agesDAW= {{20, 20, 19, 19, 28, 17, 19},{31, 29, 24, 21, 19, 19, 19},{18, 20, 18, 23, 18}};
 
-        menor(claseDAW, edadesDAW);
+        younger(classDAW, agesDAW);
+        older(classDAW, agesDAW);
     }
-    public static void menor(String [][] claseDAW, int[][] edadesDAW) {
-        for (int i = 0; i < edadesDAW.length; i++) {
-            for(int j = 0; j < edadesDAW.length; j++) {
-
+    public static void younger(String [][] classDAW, int[][] agesDAW) {
+        int lowAge= 40;
+        int[] savePosition = new int [2];
+        for (int i = 0; i < agesDAW.length; i++) {
+            for(int j = 0; j < agesDAW[i].length; j++) {
+                if (lowAge > agesDAW[i][j]){
+                    lowAge = agesDAW[i][j];
+                    savePosition[0] = i;
+                    savePosition[1] = j;
+                }
             }
         }
+        int fila = savePosition[0] + 1;
+        int silla = savePosition[1] + 1;
+        System.out.printf("La persona menor, se situa en la fila %d silla %d su nombre es %s y tiene %d años\n\n", fila, silla, classDAW[savePosition[0]][savePosition[1]], lowAge);
+    }
+    public static void older(String [][] classDAW, int[][] agesDAW) {
+        int highAge= 0;
+        int[] savePosition = new int [2];
+        for (int i = 0; i < agesDAW.length; i++) {
+            for(int j = 0; j < agesDAW[i].length; j++) {
+                if (highAge < agesDAW[i][j]){
+                    highAge = agesDAW[i][j];
+                    savePosition[0] = i;
+                    savePosition[1] = j;
+                }
+            }
+        }
+        int fila = savePosition[0] + 1;
+        int silla = savePosition[1] + 1;
+        System.out.printf("La persona mayor, se situa en la fila %d silla %d su nombre es %s y tiene %d años\n", fila, silla, classDAW[savePosition[0]][savePosition[1]], highAge);
     }
 }
