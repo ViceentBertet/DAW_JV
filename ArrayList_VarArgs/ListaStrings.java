47c0b8bd6a5cc3b53bb948b157f672d27e4c0135
viceentbertet <perellobertetjosepvicent@gmail.com>
2023-12-18 22:48:21 +0100

    ListaStrings hasta modificar

--- ArrayList_VarArgs/ListaStrings.java
+++ ArrayList_VarArgs/ListaStrings.java
@@ -5,19 +5,20 @@ import java.util.Scanner;
 
 public class ListaStrings {
     static Scanner sc = new Scanner(System.in);
+    static ArrayList<String> list = new ArrayList<>();
     public static void main(String[] args){
-        System.out.println("**ADMINISTRADOR DE LISTAS**");
-        ArrayList<String> list = new ArrayList<>();
-        createList(list);
-        menu(list);
+        System.out.println("**ADMINISTRADOR DE LISTAS**\n");
+
+        createList();
+        menu();
     }
-    public static void createList(ArrayList<String> list){
+    public static void createList(){
         final String finish = "terminar";
         boolean exit = false;
 
         System.out.println("Introducir elementos a la lista, si quieres terminar, introduce /terminar/: ");
         while(!exit) {
-            list.add(sc.next());
+            list.add(sc.next().toLowerCase());
 
             for (int i = 0; i < list.size(); i++) {
                 if(finish.equalsIgnoreCase(list.get(i))) {
@@ -30,10 +31,10 @@ public class ListaStrings {
             }
         }
     }
-    public static void menu(ArrayList<String> list) {
+    public static void menu() {
         boolean exit = false;
 
-        System.out.println("MENU DE MANIPULACIÓN DE LISTA\n");
+        System.out.println("\nMENU DE MANIPULACIÓN DE LISTA\n");
         System.out.println("\tIntroduzca 1 para mostrar la lista");
         System.out.println("\tIntroduzca 2 para agregar elementos");
         System.out.println("\tIntroduzca 3 para buscar elemento");
@@ -48,27 +49,27 @@ public class ListaStrings {
             switch (n) {
                 case 1:
                     System.out.println("Ha seleccionado mostrar la lista");
-                    showList(list);
+                    showList();
                     System.out.print("\nIntroduce lo que quieres hacer ahora: ");
                     break;
                 case 2:
                     System.out.println("Ha seleccionado agregar elementos");
-                    createList(list);
+                    createList();
                     System.out.print("\nIntroduce lo que quieres hacer ahora: ");
                     break;
                 case 3:
                     System.out.println("Ha seleccionado buscar la lista");
-                    findInList(list);
+                    findInList();
                     System.out.print("\nIntroduce lo que quieres hacer ahora: ");
                     break;
                 case 4:
                     System.out.println("Ha seleccionado eliminar elemento");
-
+                    deleteInList();
                     System.out.print("\nIntroduce lo que quieres hacer ahora: ");
                     break;
                 case 5:
                     System.out.println("Ha seleccionado modificar elemento");
-
+                    modifyInList();
                     System.out.print("\nIntroduce lo que quieres hacer ahora: ");
                     break;
                 case 6:
@@ -91,7 +92,7 @@ public class ListaStrings {
             }
         }
     }
-    public static void showList(ArrayList<String> list) {
+    public static void showList() {
         for (int i = 0; i < list.size(); i++) {
             if (i != list.size() - 1) {
                 System.out.print(list.get(i) + ", ");
@@ -100,7 +101,7 @@ public class ListaStrings {
             }
         }
     }
-    public static void findInList(ArrayList<String> list) {
+    public static void findInList() {
         System.out.println("Introduce como quieres buscar el elemento.");
         System.out.println("\tIntroduzca 1 para buscar por el valor introducido");
         System.out.println("\tIntroduzca 2 para buscar con el indice de la lista");
@@ -108,11 +109,86 @@ public class ListaStrings {
 
         if (n == 1) {
             System.out.println("Has seleccionado buscar por el valor introducido");
+            System.out.print("Introduce el valor a buscar: ");
+            String buscar = sc.next().toLowerCase();
+
+            if (!list.contains(buscar)) {
+                System.out.println("Valor no encontrado");
+            } else {
+                System.out.println("La palabra" + buscar + " esta en la posición " + list.indexOf(buscar));
+            }
         }else if (n == 2) {
             System.out.println("Has seleccionado buscar con el indice de la lista");
+            System.out.print("Introduce el valor a buscar: ");
+            int buscar = sc.nextInt();
+
+            if (buscar >= list.size()) {
+                System.out.println("El indice esta fuera de la lista");
+            } else {
+                System.out.println("La palabra" + list.get(buscar) + " esta en la posición " + buscar);
+            }
         } else {
             System.out.print("ERROR: Valor no comprendido. Vuelve a introducir:");
         }
+    }
+    public static void deleteInList() {
+        System.out.println("Introduce como quieres eliminar el elemento.");
+        System.out.println("\tIntroduzca 1 para eliminar por el valor introducido");
+        System.out.println("\tIntroduzca 2 para eliminar con el indice de la lista");
+        int n = sc.nextInt();
 
+        if (n == 1) {
+            System.out.println("Has seleccionado eliminar por el valor introducido");
+            System.out.print("Introduce el valor a eliminar: ");
+            String buscar = sc.next().toLowerCase();
+
+            if (!list.contains(buscar)) {
+                System.out.println("Valor no encontrado");
+            } else {
+                list.remove(buscar);
+            }
+        }else if (n == 2) {
+            System.out.println("Has seleccionado eliminar con el indice de la lista");
+            System.out.print("Introduce el valor a eliminar: ");
+            int buscar = sc.nextInt();
+
+            if (buscar >= list.size()) {
+                System.out.println("El indice esta fuera de la lista");
+            } else {
+                list.remove(buscar);
+            }
+        } else {
+            System.out.print("ERROR: Valor no comprendido. Vuelve a introducir:");
+        }
+    }
+    public static void modifyInList() {
+        System.out.println("Introduce como quieres eliminar el elemento.");
+        System.out.println("\tIntroduzca 1 para eliminar por el valor introducido");
+        System.out.println("\tIntroduzca 2 para eliminar con el indice de la lista");
+        int n = sc.nextInt();
+
+        if (n == 1) {
+            System.out.println("Has seleccionado eliminar por el valor introducido");
+            System.out.print("Introduce el valor a eliminar: ");
+            String buscar = sc.next().toLowerCase();
+
+            if (!list.contains(buscar)) {
+                System.out.println("Valor no encontrado");
+            } else {
+                list.remove(buscar);
+            }
+        }else if (n == 2) {
+            System.out.println("Has seleccionado eliminar con el indice de la lista");
+            System.out.print("Introduce el valor a eliminar: ");
+            int buscar = sc.nextInt();
+
+            if (buscar >= list.size()) {
+                System.out.println("El indice esta fuera de la lista");
+            } else {
+                list.remove(buscar);
+            }
+        } else {
+            System.out.print("ERROR: Valor no comprendido. Vuelve a introducir:");
+        }
     }
 }
