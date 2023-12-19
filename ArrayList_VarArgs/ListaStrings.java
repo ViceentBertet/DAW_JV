2dadec2d0d743b25225dae32e7675aad4b512345
ViceentBertet <perellobertetjosepvicent@gmail.com>
2023-12-19 11:25:46 +0100

    Lista Strings creació

--- ArrayList_VarArgs/ListaStrings.java
+++ ArrayList_VarArgs/ListaStrings.java
@@ -7,90 +7,91 @@ import java.util.Scanner;
 public class ListaStrings {
     static Scanner sc = new Scanner(System.in);
     static ArrayList<String> list = new ArrayList<>();
-    public static void main(String[] args){
-        System.out.println("**ADMINISTRADOR DE LISTAS**\n");
 
+    public static void main(String[] args) {
+
+        System.out.println("**ADMINISTRADOR DE LISTAS**\n");
         createList();
-        menu();
+        int n = 1;
+
+            System.out.println("\nMENU DE MANIPULACIÓN DE LISTA\n");
+            System.out.println("\tIntroduzca 1 para mostrar la lista");
+            System.out.println("\tIntroduzca 2 para agregar elementos");
+            System.out.println("\tIntroduzca 3 para buscar elemento");
+            System.out.println("\tIntroduzca 4 para eliminar elemento");
+            System.out.println("\tIntroduzca 5 para modificar elemento");
+            System.out.println("\tIntroduzca 6 para limpiar la lista");
+            System.out.println("\tIntroduzca 7 para ordenar la lista");
+            System.out.println("\tIntroduzca 8 para salir del programa");
+
+        do {
+            n = sc.nextInt();
+            if (n < 1 || n > 8) {
+                System.out.println("No ha introducido una opci♥n de menú válida. Int♥ntelo de nuevo.");
+            } else {
+                processMenu(n);
+            }
+        } while (n != 8);
     }
-    public static void createList(){
-        final String finish = "terminar";
+
+    public static void createList() {
+        final String FINISH = "terminar";
         boolean exit = false;
 
-        System.out.println("Introducir elementos a la lista, si quieres terminar, introduce /terminar/: ");
-        while(!exit) {
-            list.add(sc.next().toLowerCase());
+        System.out.println("Introducir elementos a la lista, si quieres terminar de introducir elementos, introduce /terminar/: ");
+        while (!exit) {
+            String add = sc.next().toLowerCase();
 
-            for (int i = 0; i < list.size(); i++) {
-                if(finish.equalsIgnoreCase(list.get(i))) {
-                    exit = true;
-                    list.remove(list.get(i));
-
-                    System.out.println("Has introducido terminar");
-
-                }
+            if (FINISH.equalsIgnoreCase(add)) {
+                exit = true;
+                System.out.println("Has introducido terminar");
+            } else {
+                list.add(add);
             }
         }
     }
-    public static void menu() {
-        boolean exit = false;
-
-        System.out.println("\nMENU DE MANIPULACIÓN DE LISTA\n");
-        System.out.println("\tIntroduzca 1 para mostrar la lista");
-        System.out.println("\tIntroduzca 2 para agregar elementos");
-        System.out.println("\tIntroduzca 3 para buscar elemento");
-        System.out.println("\tIntroduzca 4 para eliminar elemento");
-        System.out.println("\tIntroduzca 5 para modificar elemento");
-        System.out.println("\tIntroduzca 6 para limpiar la lista");
-        System.out.println("\tIntroduzca 7 para ordenar la lista");
-        System.out.println("\tIntroduzca 8 para salir del programa");
-
-        while (!exit) {
-            int n = sc.nextInt();
-            switch (n) {
-                case 1:
-                    System.out.println("Ha seleccionado mostrar la lista");
-                    showList();
-                    System.out.print("\nIntroduce lo que quieres hacer ahora: ");
-                    break;
-                case 2:
-                    System.out.println("Ha seleccionado agregar elementos");
-                    createList();
-                    System.out.print("\nIntroduce lo que quieres hacer ahora: ");
-                    break;
-                case 3:
-                    System.out.println("Ha seleccionado buscar la lista");
-                    findInList();
-                    System.out.print("\nIntroduce lo que quieres hacer ahora: ");
-                    break;
-                case 4:
-                    System.out.println("Ha seleccionado eliminar elemento");
-                    deleteInList();
-                    System.out.print("\nIntroduce lo que quieres hacer ahora: ");
-                    break;
-                case 5:
-                    System.out.println("Ha seleccionado modificar elemento");
-                    modifyInList();
-                    System.out.print("\nIntroduce lo que quieres hacer ahora: ");
-                    break;
-                case 6:
-                    System.out.println("Ha seleccionado limpiar la lista");
-                    cleanList();
-                    System.out.print("\nIntroduce lo que quieres hacer ahora: ");
-                    break;
-                case 7:
-                    System.out.println("Ha seleccionado ordernar la lista");
-                    sortList();
-                    System.out.print("\nIntroduce lo que quieres hacer ahora: ");
-                    break;
-                case 8:
-                    System.out.println("Ha seleccionado salir del programa");
-
-                    exit = true;
-                    break;
-                default:
-                    System.out.print("ERROR: Valor no comprendido. Vuelve a introducir:");
-            }
+    public static void processMenu(int opcionMenu) {
+        switch (opcionMenu) {
+            case 1:
+                System.out.println("Ha seleccionado mostrar la lista");
+                showList();
+                System.out.print("\nIntroduce lo que quieres hacer ahora: ");
+                break;
+            case 2:
+                System.out.println("Ha seleccionado agregar elementos");
+                createList();
+                System.out.print("\nIntroduce lo que quieres hacer ahora: ");
+                break;
+            case 3:
+                System.out.println("Ha seleccionado buscar la lista");
+                findInList();
+                System.out.print("\nIntroduce lo que quieres hacer ahora: ");
+                break;
+            case 4:
+                System.out.println("Ha seleccionado eliminar elemento");
+                deleteInList();
+                System.out.print("\nIntroduce lo que quieres hacer ahora: ");
+                break;
+            case 5:
+                System.out.println("Ha seleccionado modificar elemento");
+                modifyInList();
+                System.out.print("\nIntroduce lo que quieres hacer ahora: ");
+                break;
+            case 6:
+                System.out.println("Ha seleccionado limpiar la lista");
+                cleanList();
+                System.out.print("\nIntroduce lo que quieres hacer ahora: ");
+                break;
+            case 7:
+                System.out.println("Ha seleccionado ordernar la lista");
+                sortList();
+                System.out.print("\nIntroduce lo que quieres hacer ahora: ");
+                break;
+            case 8:
+                System.out.println("Ha seleccionado salir del programa");
+                break;
+            default:
+                System.out.print("ERROR: Valor no comprendido. Vuelve a introducir:");
         }
     }
     public static void showList() {
@@ -102,6 +103,7 @@ public class ListaStrings {
             }
         }
     }
+
     public static void findInList() {
         System.out.println("Introduce como quieres buscar el elemento.");
         System.out.println("\tIntroduzca 1 para buscar por el valor introducido");
@@ -118,7 +120,7 @@ public class ListaStrings {
             } else {
                 System.out.println("La palabra" + find + " esta en la posición " + list.indexOf(find));
             }
-        }else if (n == 2) {
+        } else if (n == 2) {
             System.out.println("Has seleccionado buscar con el indice de la lista");
             System.out.print("Introduce el indice a buscar: ");
             int find = sc.nextInt();
@@ -132,6 +134,7 @@ public class ListaStrings {
             System.out.print("ERROR: Valor no comprendido. Vuelve a introducir:");
         }
     }
+
     public static void deleteInList() {
         System.out.println("Introduce como quieres eliminar el elemento.");
         System.out.println("\tIntroduzca 1 para eliminar por el valor introducido");
@@ -149,7 +152,7 @@ public class ListaStrings {
                 list.remove(delete);
                 System.out.println("Se ha eliminado correctamente");
             }
-        }else if (n == 2) {
+        } else if (n == 2) {
             System.out.println("Has seleccionado eliminar con el indice de la lista");
             System.out.print("Introduce el indice a eliminar: ");
             int delete = sc.nextInt();
@@ -164,6 +167,7 @@ public class ListaStrings {
             System.out.print("ERROR: Valor no comprendido. Vuelve a introducir:");
         }
     }
+
     public static void modifyInList() {
         System.out.println("Introduce como quieres eliminar el elemento.");
         System.out.println("\tIntroduzca 1 para modificar por el valor introducido");
@@ -174,7 +178,7 @@ public class ListaStrings {
             System.out.println("Has seleccionado modificar por el valor introducido");
             System.out.print("Introduce el valor a modificar: ");
             String modify = sc.next().toLowerCase();
-            System.out.println("Introduce el valor que quieres que permanezca");
+            System.out.print("Introduce el valor que quieres que permanezca: ");
             String newModify = sc.next().toLowerCase();
 
             if (!list.contains(modify)) {
@@ -183,11 +187,11 @@ public class ListaStrings {
                 list.set(list.indexOf(modify), newModify);
                 System.out.println("Se ha modificado correctamente");
             }
-        }else if (n == 2) {
+        } else if (n == 2) {
             System.out.println("Has seleccionado modificar con el indice de la lista");
             System.out.print("Introduce el indice a modificar: ");
             int modify = sc.nextInt();
-            System.out.println("Introduce el valor que quieres que permanezca");
+            System.out.print("Introduce el valor que quieres que permanezca: ");
             String newModify = sc.next().toLowerCase();
 
             if (modify >= list.size()) {
@@ -199,11 +203,12 @@ public class ListaStrings {
         } else {
             System.out.print("ERROR: Valor no comprendido. Vuelve a introducir:");
         }
-
     }
-    public static void cleanList(){
+
+    public static void cleanList() {
         list.clear();
     }
+
     public static void sortList() {
         Collections.sort(list);
         showList();
