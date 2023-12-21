699e638d5c4fdb10ac45c9ad70b60ac4cc9b9bc5
viceentbertet <perellobertetjosepvicent@gmail.com>
2023-12-21 22:19:40 +0100

    Calculadora Promedio

---
+++ ArrayList_VarArgs/CalcularPromedio.java
@@ -0,0 +1,15 @@
+package ArrayList_VarArgs;
+
+public class CalcularPromedio {
+    public static void main(String[] args) {
+        int suma = sumaVarArgs();
+        System.out.println("El resultado de la suma es: " + suma);
+    }
+    public static int sumaVarArgs(int... numeros) {
+        int suma = 0;
+        for (int numero : numeros) {
+            suma += numero;
+        }
+        return suma;
+    }
+}
