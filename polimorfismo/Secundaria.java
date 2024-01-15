c9cfd71b5558a814687284da7e3d1bcf3c731487
viceentbertet <perellobertetjosepvicent@gmail.com>
2024-01-15 22:18:56 +0100

    polimorfismo

---
+++ polimorfismo/Secundaria.java
@@ -0,0 +1,21 @@
+package polimorfismo;
+
+import java.util.Optional;
+
+public class Secundaria extends Estudiante {
+    private String optativa;
+    public Secundaria (String nombre, String edad, String aficion, String asignaturaFav, boolean vestimenta, String optativa){
+        super(nombre, edad, aficion, asignaturaFav, vestimenta);
+        this.optativa = optativa;
+    }
+    public String getOptativa() {
+        return optativa;
+    }
+    public void setOptativa(String optativa) {
+        this.optativa = optativa;
+    }
+    public void mostrarInformacion() {
+        super.mostrarInformacion();
+        System.out.println(". Por ultimo, su optativa es " + this.optativa);
+    }
+}
