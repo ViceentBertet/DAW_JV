804dbdb6fbc000be4feff89ed2c4c7dd87088f37
ViceentBertet <perellobertetjosepvicent@gmail.com>
2024-02-13 13:29:53 +0100

    Integración ahorcado

--- objetos_clases/multiplication_game/GestionJuegos.java
+++ objetos_clases/multiplication_game/GestionJuegos.java
@@ -1,12 +1,43 @@
 package objetos_clases.multiplication_game;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class GestionJuegos {
+    private static Scanner sc = new Scanner(System.in);
+    private static ArrayList<String> juegos = new ArrayList<>();
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("¿Cuantos jugadores (2/3)? ");
+        juegos.add("Multiplication Game");
+        juegos.add("El ahorcado");
 
+
+        System.out.println("Introduce el n del juego que quieres jugar");
+        mostrarJuegos();
+        int jugar = sc.nextInt();
+        switch (jugar) {
+            case 1:
+                System.out.println("Has elegido " + juegos.get(jugar));
+                multiGame();
+                break;
+            case 2:
+                System.out.println("Has elegido " + juegos.get(jugar));
+                //jugar al ahorcado
+                break;
+            default:
+                System.out.println("El n elegido no corresponde a ningun juego.");
+        }
+
+
+    }
+    public static void mostrarJuegos(){
+        int nJuego;
+        for (int i = 0; i < juegos.size(); i++) {
+            nJuego = i + 1;
+            System.out.println(nJuego + ": " + juegos.get(i));
+        }
+    }
+    public static void multiGame(){
+        System.out.print("¿Cuantos jugadores (2/3)? ");
         int nPlayers = sc.nextInt();
         if (nPlayers != 2 && nPlayers != 3) {
             System.out.println("Numero de jugadores incorrecto");
